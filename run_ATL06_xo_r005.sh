e8754af2fe0efece6126f3be30ffc41bc3b2f12d
Denis Felikson <dfelikson@gmail.com>
2023-04-04 09:21:56 -0400

    Fixed inconsistent variable names; added a test_run command line option

--- run_ATL06_xo_r005.sh
+++ run_ATL06_xo_r005.sh
@@ -20,7 +20,7 @@ EOF_USAGE
 #-------------------------------------------------------------------
 #
 icesheet=Arctic
-rel=005
+release=005
 single_cycle=0
 cycle_name=""
 work_dir=/ATL06_xo/mjohnson
@@ -40,6 +40,7 @@ while [ $# -gt 0 ] ; do
     -s|--single) single_cycle=1;;
     --work_dir) shift; work_dir="$1";;
     --script_path) shift; script_path="$1";;
+    --test_run) test_run=1;;
     -*) echo ""; echo 'ERROR: Invalid argument'; usage; exit 3;;
   esac
   shift
@@ -71,7 +72,7 @@ fi
 if [ ${single_cycle} == 0 ]; then ##{{{
   for cycle in ${cycles[@]}; do
     cycle_name+=${cycle}
-    cycle_dir=${icesheet}/r${rel}/c${cycle_name}
+    cycle_dir=${icesheet}/r${release}/c${cycle_name}
 
   if [ ${xtra_seg} != 0 ] ; then
     seg_name=_${xtra_seg}_extra_segments  
@@ -97,40 +98,43 @@ if [ ${single_cycle} == 0 ]; then ##{{{
   echo single_cycle: $single_cycle
   echo work_dir: $work_dir
   echo script_path: $script_path
+  echo test_run: $test_run
   echo -- Setup --
   echo
 
-  echo -- Git repository checks --
-  basedir=$(dirname $0)
-  basedir_git_log=$(cd $basedir && git log -1 | head -n 1)
-  if [[ "$basedir_git_log" != *"commit"* ]]; then
-    echo The directory $basedir is not a git repository! Cannot log commit hash. Exiting! >&2
-    exit
-  fi
-  basedir_git_status=$(cd $basedir && git status --porcelain)
-  if [[ ! -z "$basedir_git_status" ]]; then
-     echo The code in the ATL06-Crossover-Code repository "(directory: $basedir)" has the following uncommitted changes. Exiting! >&2
-     echo "$basedir_git_status" >&2
-     exit
-  fi
-  pointCollectiondir=${script_path}/pointCollection
-  pointCollection_git_log=$(cd $pointCollectiondir && git log -1 | head -n 1)
-  if [[ "$pointCollection_git_log" != *"commit"* ]]; then
-    echo The directory $pointCollectiondir is not a git repository! Cannot log commit hash. Exiting! >&2
-    exit
-  fi
-  pointCollectiondir_git_status=$(cd $pointCollectiondir && git status --porcelain)
-  if [[ ! -z "$pointCollectiondir_git_status" ]]; then
-     echo The code in the pointCollection repository "(directory: $pointCollectiondir)" has the following uncommitted changes. Exiting! >&2
-     echo "$pointCollectiondir_git_status" >&2
-     exit
-  fi
+  if [ "$test_run" != "1" ]; then
+    echo -- Git repository checks --
+    basedir=$(dirname $0)
+    basedir_git_log=$(cd $basedir && git log -1 | head -n 1)
+    if [[ "$basedir_git_log" != *"commit"* ]]; then
+      echo The directory $basedir is not a git repository! Cannot log commit hash. Exiting! >&2
+      exit
+    fi
+    basedir_git_status=$(cd $basedir && git status --porcelain)
+    if [[ ! -z "$basedir_git_status" ]]; then
+       echo The code in the ATL06-Crossover-Code repository "(directory: $basedir)" has the following uncommitted changes. Exiting! >&2
+       echo "$basedir_git_status" >&2
+       exit
+    fi
+    pointCollectiondir=${script_path}/pointCollection
+    pointCollection_git_log=$(cd $pointCollectiondir && git log -1 | head -n 1)
+    if [[ "$pointCollection_git_log" != *"commit"* ]]; then
+      echo The directory $pointCollectiondir is not a git repository! Cannot log commit hash. Exiting! >&2
+      exit
+    fi
+    pointCollectiondir_git_status=$(cd $pointCollectiondir && git status --porcelain)
+    if [[ ! -z "$pointCollectiondir_git_status" ]]; then
+       echo The code in the pointCollection repository "(directory: $pointCollectiondir)" has the following uncommitted changes. Exiting! >&2
+       echo "$pointCollectiondir_git_status" >&2
+       exit
+    fi
 
-  echo This processing uses the following code versions:
-  echo " " ATL06-Crossover-Code: $basedir_git_log
-  echo " " pointCollection: $pointCollection_git_log
-  echo -- Git repository checks --
-  echo
+    echo This processing uses the following code versions:
+    echo " " ATL06-Crossover-Code: $basedir_git_log
+    echo " " pointCollection: $pointCollection_git_log
+    echo -- Git repository checks --
+    echo
+  fi
   ##}}}
 
   for cycle in ${cycles[@]}; do
@@ -141,7 +145,7 @@ if [ ${single_cycle} == 0 ]; then ##{{{
 
     for reg in "${regions[@]}"; do
 
-      for file in `ls /cooler/I2-ASAS/rel${rel}/ATL06/ATL06_*_????${cycle}${reg}_${rel}_*.h5`; do
+      for file in `ls /cooler/I2-ASAS/rel${release}/ATL06/ATL06_*_????${cycle}${reg}_${release}_*.h5`; do
         newfile=`echo $file | rev | cut -d'/' -f1 | rev`
         ln -s $file ${work_dir}/${cycle_dir}/$newfile
       done
@@ -157,7 +161,7 @@ if [ ${single_cycle} == 0 ]; then ##{{{
      file_template=`echo $file_v01 | rev | cut -d'/' -f1 | rev | cut -d'_' -f1-4`
      n_files_all_versions=`ls -l ${work_dir}/${cycle_dir}/$file_template*h5 | wc -l`
      if [ $n_files_all_versions -gt 1 ]; then
-        file_highest_version=`ls -ltr /cooler/I2-ASAS/rel${rel}/ATL06/$file_template*h5 | tail -1 | awk '{print $9}'`
+        file_highest_version=`ls -ltr /cooler/I2-ASAS/rel${release}/ATL06/$file_template*h5 | tail -1 | awk '{print $9}'`
         filename_highest_version=`echo $file_highest_version | rev | cut -d'/' -f1 | rev`
         rm -rf ${work_dir}/${cycle_dir}/$file_template*h5
         ln -s $file_highest_version ${work_dir}/${cycle_dir}/$filename_highest_version
@@ -166,19 +170,21 @@ if [ ${single_cycle} == 0 ]; then ##{{{
    done
 
   # Tile
-  ./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path > out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.out 2> out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.err
+  ./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path > out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_c${cycle_name}${seg_name}.out 2> out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_c${cycle_name}${seg_name}.err
+  #echo ./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path
   # Crossovers
-  ./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path > out_and_err_files/cross_ATL06_tile_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.out 2> out_and_err_files/cross_ATL06_tile_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.err
+  ./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path > out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_c${cycle_name}${seg_name}.out 2> out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_c${cycle_name}${seg_name}.err
+  #./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path
 
   # Cleanup
-  \rm -rf ${work_dir}/${cycle_dir}
+  #\rm -rf ${work_dir}/${cycle_dir}
 ##}}}
 else ##{{{
   for cycle in ${cycles[@]}; do
     cycle_name+=${cycle}
   done
 
-  cycle_dir=${icesheet}/r${rel}/c${cycle_name}
+  cycle_dir=${icesheet}/r${release}/c${cycle_name}
 
   if [ ${xtra_seg} != 0 ] ; then
     seg_name=_${xtra_seg}_extra_segments  
@@ -203,40 +209,43 @@ else ##{{{
   echo single_cycle: $single_cycle
   echo work_dir: $work_dir
   echo script_path: $script_path
+  echo test_run: $test_run
   echo -- Setup --
   echo
 
-  echo -- Git repository checks --
-  basedir=$(dirname $0)
-  basedir_git_log=$(cd $basedir && git log -1 | head -n 1)
-  if [[ "$basedir_git_log" != *"commit"* ]]; then
-    echo The directory $basedir is not a git repository! Cannot log commit hash. Exiting! >&2
-    exit
-  fi
-  basedir_git_status=$(cd $basedir && git status --porcelain)
-  if [[ ! -z "$basedir_git_status" ]]; then
-     echo The code in the ATL06-Crossover-Code repository "(directory: $basedir)" has the following uncommitted changes. Exiting! >&2
-     echo "$basedir_git_status" >&2
-     exit
-  fi
-  pointCollectiondir=${script_path}/pointCollection
-  pointCollection_git_log=$(cd $pointCollectiondir && git log -1 | head -n 1)
-  if [[ "$pointCollection_git_log" != *"commit"* ]]; then
-    echo The directory $pointCollectiondir is not a git repository! Cannot log commit hash. Exiting! >&2
-    exit
-  fi
-  pointCollectiondir_git_status=$(cd $pointCollectiondir && git status --porcelain)
-  if [[ ! -z "$pointCollectiondir_git_status" ]]; then
-     echo The code in the pointCollection repository "(directory: $pointCollectiondir)" has the following uncommitted changes. Exiting! >&2
-     echo "$pointCollectiondir_git_status" >&2
-     exit
-  fi
+  if [ "$test_run" != "1" ]; then
+    echo -- Git repository checks --
+    basedir=$(dirname $0)
+    basedir_git_log=$(cd $basedir && git log -1 | head -n 1)
+    if [[ "$basedir_git_log" != *"commit"* ]]; then
+      echo The directory $basedir is not a git repository! Cannot log commit hash. Exiting! >&2
+      exit
+    fi
+    basedir_git_status=$(cd $basedir && git status --porcelain)
+    if [[ ! -z "$basedir_git_status" ]]; then
+       echo The code in the ATL06-Crossover-Code repository "(directory: $basedir)" has the following uncommitted changes. Exiting! >&2
+       echo "$basedir_git_status" >&2
+       exit
+    fi
+    pointCollectiondir=${script_path}/pointCollection
+    pointCollection_git_log=$(cd $pointCollectiondir && git log -1 | head -n 1)
+    if [[ "$pointCollection_git_log" != *"commit"* ]]; then
+      echo The directory $pointCollectiondir is not a git repository! Cannot log commit hash. Exiting! >&2
+      exit
+    fi
+    pointCollectiondir_git_status=$(cd $pointCollectiondir && git status --porcelain)
+    if [[ ! -z "$pointCollectiondir_git_status" ]]; then
+       echo The code in the pointCollection repository "(directory: $pointCollectiondir)" has the following uncommitted changes. Exiting! >&2
+       echo "$pointCollectiondir_git_status" >&2
+       exit
+    fi
 
-  echo This processing uses the following code versions:
-  echo " " ATL06-Crossover-Code: $basedir_git_log
-  echo " " pointCollection: $pointCollection_git_log
-  echo -- Git repository checks --
-  echo
+    echo This processing uses the following code versions:
+    echo " " ATL06-Crossover-Code: $basedir_git_log
+    echo " " pointCollection: $pointCollection_git_log
+    echo -- Git repository checks --
+    echo
+  fi
   ##}}}
 
   for cycle in ${cycles[@]}; do
@@ -247,7 +256,7 @@ else ##{{{
 
     for reg in "${regions[@]}"; do
 
-      for file in `ls /cooler/I2-ASAS/rel${rel}/ATL06/ATL06_*_????${cycle}${reg}_${rel}_*.h5`; do
+      for file in `ls /cooler/I2-ASAS/rel${release}/ATL06/ATL06_*_????${cycle}${reg}_${release}_*.h5`; do
         newfile=`echo $file | rev | cut -d'/' -f1 | rev`
         ln -s $file ${work_dir}/${cycle_dir}/$newfile
       done
@@ -261,7 +270,7 @@ else ##{{{
       file_template=`echo $file_v01 | rev | cut -d'/' -f1 | rev | cut -d'_' -f1-4`
       n_files_all_versions=`ls -l ${work_dir}/${cycle_dir}/$file_template*h5 | wc -l`
       if [ $n_files_all_versions -gt 1 ]; then
-         file_highest_version=`ls -ltr /cooler/I2-ASAS/rel${rel}/ATL06/$file_template*h5 | tail -1 | awk '{print $9}'`
+         file_highest_version=`ls -ltr /cooler/I2-ASAS/rel${release}/ATL06/$file_template*h5 | tail -1 | awk '{print $9}'`
          filename_highest_version=`echo $file_highest_version | rev | cut -d'/' -f1 | rev`
          rm -rf ${work_dir}/${cycle_dir}/$file_template*h5
          ln -s $file_highest_version ${work_dir}/${cycle_dir}/$filename_highest_version
@@ -269,13 +278,15 @@ else ##{{{
     
    done
 
-     # Tile
-    ./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path > out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.out 2> out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.err
-     # Crossovers
-    ./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path > out_and_err_files/cross_ATL06_tile_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.out 2> out_and_err_files/cross_ATL06_tile_${icesheet}_rel${rel}_c${cycle_name}${seg_name}.err
+    # Tile
+    ./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path > out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_c${cycle_name}${seg_name}.out 2> out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_c${cycle_name}${seg_name}.err
+    #echo make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path
+    # Crossovers
+    ./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path > out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_c${cycle_name}${seg_name}.out 2> out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_c${cycle_name}${seg_name}.err
+    #echo cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path
 
      # Cleanup
-     \rm -rf ${work_dir}/${cycle_dir}
+     #\rm -rf ${work_dir}/${cycle_dir}
   done   
 fi
 ##}}}
