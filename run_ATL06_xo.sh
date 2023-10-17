eeb757b077faa00eb4ddbfe4ac41654fa6d79575
Denis Felikson <dfelikson@gmail.com>
2023-10-17 08:24:21 -0400

    Added new functionality to process crossovers by start date and end date

--- run_ATL06_xo.sh
+++ run_ATL06_xo.sh
@@ -37,6 +37,8 @@ while [ $# -gt 0 ] ; do
       -r|--release) shift; release="$1";;
       -c|--cycle) shift; cycles="${cycles} $1";;
       -x|--xtra) shift; xtra_seg="$1";;
+      -sd|--start_date) shift; start_date="$1";;
+      -ed|--end_date) shift; end_date="$1";;
       --work_dir) shift; work_dir="$1";;
       --script_path) shift; script_path="$1";;
       --test_run) test_run=1;;
@@ -51,17 +53,11 @@ done
 #fi
 
 #
-# Determine cycles to be used
+# Set default argument values
 #
-if [ "${cycles}" == "" ] ; then
-   cycles=(14)
-fi
 if [ "${xtra_seg}" == "" ] ; then
    xtra_seg=0
-   seg_name=""
 fi
-
-
 if [ "$icesheet" == "Arctic" ]; then
    hemisphere=1
    regions=( 03 04 05 )
@@ -73,15 +69,30 @@ else
    exit
 fi
 
-for cycle in ${cycles[@]}; do
-   cycle_name+=${cycle}
-done
+#
+# Determine cycles to be used
+#
+if [ "${cycles}" != "" ] ; then
+  cycle_name=c
+  for cycle in ${cycles[@]}; do
+     cycle_name+=${cycle}
+  done
+  cycle_dir=${icesheet}/r${release}/c${cycle_name}
+  out_err_suffix=c${cycle_name}
 
-cycle_dir=${icesheet}/r${release}/c${cycle_name}
+elif [ "${start_date}" != "" ] && [ "${end_date}" != "" ]; then
+   start_time=000000
+   end_time=235959
+   cycle_name=t${start_date}-${end_date}
+   cycle_dir=${icesheet}/r${release}/${cycle_name}
+   start_date_time=$start_date$start_time
+   end_date_time=$end_date$end_time
+   out_err_suffix=t${start_date}-${end_date}
+fi
 
 if [ ${xtra_seg} != 0 ] ; then
-   seg_name=_${xtra_seg}_extra_segments  
-   cycle_dir+=${seg_name}
+   cycle_dir+=_${xtra_seg}_extra_segments
+   out_err_suffix+=_${xtra_seg}_extra_segments
 fi      
 
 if [ ! -d ${work_dir}/${cycle_dir} ]; then
@@ -93,7 +104,7 @@ if [ ! -d /ATL06_xo/${cycle_dir} ]; then
 fi	
 
 # Echo the setup into a file in /ATL06_xo/$cycle_dir ##{{{
-exec > /ATL06_xo/$cycle_dir/run_ATL06_xo_r005.out
+exec > /ATL06_xo/$cycle_dir/run_ATL06_xo.out
 echo -- Setup --
 echo icesheet: $icesheet
 echo release: $release
@@ -143,15 +154,29 @@ fi
 
 echo "[`date`] Starting run script"
 
-for cycle in ${cycles[@]}; do
-   echo " Linking ATL06 files for cycle $cycle"
+if [ "${cycles}" != "" ] ; then
+   # Link ATL06 by cycle
+   for cycle in ${cycles[@]}; do
+      echo " Linking ATL06 files for cycle $cycle"
+      for reg in "${regions[@]}"; do
+         for file in `ls /cooler/I2-ASAS/rel${release}/ATL06/ATL06_*_????${cycle}${reg}_${release}_*.h5`; do
+            newfile=`echo $file | rev | cut -d'/' -f1 | rev`
+            ln -s $file ${work_dir}/${cycle_dir}/$newfile
+         done
+      done
+   done
+elif [ "${start_date}" != "" ] && [ "${end_date}" != "" ]; then
+   # Link ATL06s by time range
+   echo " Linking by time range $start_date_time-$end_date_time"
    for reg in "${regions[@]}"; do
-      for file in `ls /cooler/I2-ASAS/rel${release}/ATL06/ATL06_*_????${cycle}${reg}_${release}_*.h5`; do
+      file_list=`ls -l /cooler/I2-ASAS/rel${release}/ATL06/ATL06_*_??????${reg}_${release}_*.h5 | \
+         awk '{print $9}' | awk -v start_date_time=$start_date_time -v end_date_time=$end_date_time -F '_' '{if ($2>start_date_time && $2<end_date_time) print $0}'`
+      for file in $file_list; do
          newfile=`echo $file | rev | cut -d'/' -f1 | rev`
          ln -s $file ${work_dir}/${cycle_dir}/$newfile
       done
    done
-done
+fi
 echo "[`date`] Linking finished"
 
 # Look for multiple versions of the same file
@@ -168,12 +193,12 @@ done
 
 # Tile
 echo "[`date`] Making tiles"
-./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path > out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_c${cycle_name}${seg_name}.out 2> out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_c${cycle_name}${seg_name}.err
+./make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path > out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_${out_err_suffix}.out 2> out_and_err_files/make_ATL06_tiles_iceproc_${icesheet}_rel${release}_${out_err_suffix}.err
 #echo make_ATL06_tiles_iceproc.sh $hemisphere $work_dir/${cycle_dir} $script_path
 
 # Crossovers
 echo "[`date`] Finding crossovers"
-./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path > out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_c${cycle_name}${seg_name}.out 2> out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_c${cycle_name}${seg_name}.err
+./cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path > out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_${out_err_suffix}.out 2> out_and_err_files/cross_ATL06_tile_${icesheet}_rel${release}_${out_err_suffix}.err
 #echo cross_ATL06_tile.sh $hemisphere $work_dir/${cycle_dir}/tiles /ATL06_xo/${cycle_dir} ${xtra_seg} $script_path
 
 # Cleanup
